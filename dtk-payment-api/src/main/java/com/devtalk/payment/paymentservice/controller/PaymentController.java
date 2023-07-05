e1fba52862aef4e68465c34529f4716d2c94e22f
Oniqued <zktl3575@gmail.com>
2023-07-05 20:25:35 +0900

    결제 컨트롤러 테스트 작성

--- dtk-payment-api/src/main/java/com/devtalk/payment/paymentservice/controller/PaymentController.java
+++ dtk-payment-api/src/main/java/com/devtalk/payment/paymentservice/controller/PaymentController.java
@@ -1,6 +1,15 @@
 package com.devtalk.payment.paymentservice.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/payment")
 public class PaymentController {
 
+    @PostMapping("")
+    public String payment() {
+        return "hello";
+    }
 }
