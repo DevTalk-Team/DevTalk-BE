134b18e7e12fd7cd20a22253938ae5597675bcac
zabcd121 <zabcd121@naver.com>
2023-07-09 19:56:58 +0900

    Delete ConsultationRepository.java

--- dtk-consultation-api/src/main/java/com/devtalk/consultation/consultationservice/consultation/application/port/out/repository/ConsultationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.devtalk.consultation.consultationservice.consultation.application.port.out.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ConsultationRepository extends JpaRepository<Consultation, Long> {
-}
