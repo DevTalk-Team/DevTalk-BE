5e537225f866fc2351e18522a5ac1b1cb81be771
Kyungrim Park <pkl4693@naver.com>
2023-08-23 00:03:51 +0900

    Config: Jwt 관련

--- dtk-member-api/src/main/java/com/devtalk/member/memberservice/global/jwt/JwtTokenProvider.java
+++ dtk-member-api/src/main/java/com/devtalk/member/memberservice/global/jwt/JwtTokenProvider.java
@@ -1,38 +1,126 @@
 package com.devtalk.member.memberservice.global.jwt;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import com.devtalk.member.memberservice.global.error.ErrorCode;
+import com.devtalk.member.memberservice.global.error.exception.TokenException;
+import com.devtalk.member.memberservice.member.application.port.in.dto.TokenDto;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
+import java.security.Key;
+import java.util.Base64;
 import java.util.Date;
 
+@Slf4j
 @Component
-public class JwtTokenProvider {
+public class JwtTokenProvider implements InitializingBean {
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+    public static final String BEARER = "Bearer ";
 
-    @Value("${jwt.token.secret}")
-    private String secretKey;
-    private long expiredTime = 60 * 60 * 1000; // 1시간
+    private final MemberDetailsService memberDetailsService;
+    private final String secretKey;
+    private final long accessTokenValidity;
+    private final long refreshTokenValidity;
 
-    public String generateToken(String email) {
+    private Key key;
 
-        Claims claims = Jwts.claims();
-        claims.put("email", email);
-
-        Date now = new Date(System.currentTimeMillis());
-
-        return Jwts.builder()
-                .setSubject("authorization") // 토큰 용도
-                .setClaims(claims)
-                .setIssuedAt(now)
-                .setExpiration(new Date(now.getTime() + expiredTime))
-                .signWith(SignatureAlgorithm.HS256, secretKey)
-                .compact();
+    public JwtTokenProvider(@Value("${jwt.token.secret}") String secretKey,
+                            @Value("${jwt.token.access-token-validity-in-seconds}") long accessTokenValidity,
+                            @Value("${jwt.token.refresh-token-validity-in-seconds}") long refreshTokenValidity,
+                            MemberDetailsService memberDetailsService) {
+        this.secretKey = secretKey;
+        this.accessTokenValidity = accessTokenValidity;
+        this.refreshTokenValidity = refreshTokenValidity;
+        this.memberDetailsService = memberDetailsService;
     }
 
-//    public Authentication getAuthentication(String token) {
-//
-//    }
+    @Override
+    public void afterPropertiesSet() throws Exception { // init(), 시크릿 키 설정
+        log.info("[afterPropertiesSet] key 초기화 시작");
+        String encodedKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+        key = Keys.hmacShaKeyFor(encodedKey.getBytes());
+        log.info("[afterPropertiesSet] key 초기화 완료");
+    }
+
+    /* 토큰 생성 */
+    @Transactional
+    public TokenDto createToken(String email, String authorities) {
+        log.info("[createToken] 토큰 생성 시작");
+        Claims claims = Jwts.claims();
+        claims.put("email", email);
+        claims.put("authorities", authorities);
+        Date now = new Date();
+
+        String accessToken = Jwts.builder()
+                .setSubject("access-token")
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime() + accessTokenValidity))
+                .signWith(SignatureAlgorithm.HS512, key)
+                .compact();
+
+        String refreshToken = Jwts.builder()
+                .setSubject("refresh-token")
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime() + refreshTokenValidity))
+                .signWith(SignatureAlgorithm.HS512, key)
+                .compact();
+
+        log.info("[createToken] 토큰 생성 완료");
+        return new TokenDto(accessToken, refreshToken);
+    }
+
+    /* 토큰으로부터 정보 추출 */
+    public Authentication getAuthentication(String token) {
+        log.info("[getAuthentication] 토큰 인증 정보 조회 시작");
+        MemberDetails memberDetails = memberDetailsService.loadUserByUsername(getEmail(token));
+        log.info("[getAuthentication] 토큰 인증 정보 조회 완료, UserDetails email : {}", memberDetails.getUsername());
+        return new UsernamePasswordAuthenticationToken(memberDetails, "", memberDetails.getAuthorities());
+    }
+
+    public String getEmail(String token) {
+        log.info("[getEmail] 토큰 기반 회원 구별 정보 추출");
+        String email = Jwts.parser()
+                .setSigningKey(key)
+                .parseClaimsJws(token)
+                .getBody()
+                .get("email").toString();
+        log.info("[getEmail] 토큰 기반 회원 구별 정보 추출 완료, info : {}", email);
+        return email;
+    }
+
+    public String resolveToken(HttpServletRequest request) {
+        log.info("[resolveToken] HTTP 헤더에서 Token 값 추출");
+        return request.getHeader("X-AUTH-TOKEN");
+    }
+
+    /* 토큰 검증 */
+    public boolean validateToken(String token) {
+        log.info("[validateToken] 토큰 유효 체크 시작");
+        try {
+            Jws<Claims> claims = Jwts.parser()
+                    .setSigningKey(key)
+                    .parseClaimsJws(token);
+            return !claims.getBody().getExpiration().before(new Date());
+        } catch (SignatureException e) {
+            throw new TokenException(ErrorCode.INCORRECT_SIGNATURE);
+        } catch (ExpiredJwtException e) {
+            throw new TokenException(ErrorCode.EXPIRED_TOKEN);
+        } catch (MalformedJwtException | UnsupportedJwtException e) {
+            throw new TokenException(ErrorCode.UNSUPPORTED_TOKEN);
+        } catch (IllegalArgumentException e) {
+            throw e;
+        }
+    }
+
+    // Filter에서 사용
+//    public boolean validateAccessToken
 }
--- dtk-member-api/src/main/java/com/devtalk/member/memberservice/member/application/port/in/dto/TokenDto.java
+++ dtk-member-api/src/main/java/com/devtalk/member/memberservice/member/application/port/in/dto/TokenDto.java
@@ -1,7 +1,17 @@
 package com.devtalk.member.memberservice.member.application.port.in.dto;
 
+import com.devtalk.member.memberservice.global.jwt.AuthDto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class TokenDto {
     String accessToken;
     String refreshToken;
+
+    public static TokenDto of(String accessToken, String refreshToken) {
+        return new TokenDto(accessToken, refreshToken);
+    }
 
 }
