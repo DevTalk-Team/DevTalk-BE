a03d36a7fa54d582d0f99e2cc3dd92fc0d3f7c54
Oniqued <zktl3575@gmail.com>
2023-08-22 21:39:58 +0900

    결제 서비스 - KafkaConsumer 적용

---
+++ dtk-payment-api/src/main/java/com/devtalk/payment/global/config/CustomLocalDateTimeSerializer.java
@@ -0,0 +1,21 @@
+package com.devtalk.payment.global.config;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class CustomLocalDateTimeSerializer extends JsonSerializer<LocalDateTime> {
+
+    private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+
+    @Override
+    public void serialize(LocalDateTime value, JsonGenerator gen, SerializerProvider serializers) throws IOException, JsonProcessingException {
+        gen.writeString(formatter.format(value));
+    }
+
+}
