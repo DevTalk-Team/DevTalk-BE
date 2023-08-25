79c79a7d9b78b29e4024eea2ae9b39ab6a6db39a
Oniqued <zktl3575@gmail.com>
2023-08-26 08:28:11 +0900

    결제 서비스 - UUID 컬럼 추가

--- dtk-payment-api/src/main/resources/data.sql
+++ dtk-payment-api/src/main/resources/data.sql
@@ -1,8 +1,8 @@
 -- test Data
 
-insert into consultation (consulter, consulter_email, consultant, consultation_type, cost, process_status)
-    values ('김수현', 'zktl3575@gmail.com', '박주형', '30분 비대면 상담', 100, 'APPROVED');
 -- insert into consultation (consulter, consulter_email, consultant, consultation_type, cost, process_status)
 --     values ('김수현', 'zktl3575@gmail.com', '박주형', '30분 비대면 상담', 100, 'APPROVED');
--- insert into consultation (consulter, consulter_email, consultant, consultation_type, cost, process_status)
---     values ('김수현', 'zktl3575@gmail.com', '박주형', '30분 비대면 상담', 100, 'APPROVED');
+-- -- insert into consultation (consulter, consulter_email, consultant, consultation_type, cost, process_status)
+-- --     values ('김수현', 'zktl3575@gmail.com', '박주형', '30분 비대면 상담', 100, 'APPROVED');
+-- -- insert into consultation (consulter, consulter_email, consultant, consultation_type, cost, process_status)
+-- --     values ('김수현', 'zktl3575@gmail.com', '박주형', '30분 비대면 상담', 100, 'APPROVED');
