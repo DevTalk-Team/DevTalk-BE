d9de941e9a881db55c6f478a643b9a99dc3cadfe
Kyungrim Park <pkl4693@naver.com>
2023-08-30 16:41:53 +0900

    멤버 서비스 swagger 적용

---
+++ dtk-member-api/src/main/java/com/devtalk/member/memberservice/member/adapter/in/web/MyPageController.java
@@ -0,0 +1,36 @@
+package com.devtalk.member.memberservice.member.adapter.in.web;
+
+import com.devtalk.member.memberservice.global.success.SuccessResponse;
+import com.devtalk.member.memberservice.global.success.SuccessResponseNoResult;
+import com.devtalk.member.memberservice.member.adapter.in.web.dto.MyPageOutput;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@Tag(name = "마이페이지", description = "비밀번호 확인, 마이페이지 정보 확인&수정, 내 상담 확인")
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class MyPageController {
+
+    @PostMapping("/mypage")
+    public SuccessResponseNoResult checkPassword(HttpServletRequest request, @RequestParam String password) {
+        return new SuccessResponseNoResult();
+    }
+
+    @GetMapping("/mypage")
+    public SuccessResponse<MyPageOutput> getMyPage(HttpServletRequest request) {
+        return new SuccessResponse();
+    }
+
+    @PatchMapping("/mypage")
+    public SuccessResponse<MyPageOutput> editProfile(HttpServletRequest request) {
+        return new SuccessResponse();
+    }
+
+    @GetMapping("/mypage/consultation")
+    public SuccessResponse<MyPageOutput> getMyConsultation(HttpServletRequest request) {
+        return new SuccessResponse();
+    }
+}
---
+++ dtk-member-api/src/main/java/com/devtalk/member/memberservice/member/adapter/in/web/dto/MyPageOutput.java
@@ -0,0 +1,7 @@
+package com.devtalk.member.memberservice.member.adapter.in.web.dto;
+
+import lombok.Data;
+
+@Data
+public class MyPageOutput {
+}
