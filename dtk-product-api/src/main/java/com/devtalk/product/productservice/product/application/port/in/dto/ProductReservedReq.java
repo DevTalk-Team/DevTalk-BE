ba47a16d80409d23459c2804e40001ccdf1cedb0
goohanseo <gooruem2@naver.com>
2023-10-19 20:45:12 +0900

    product - matching kafka(상담 승인 시 예약 불가) 구현 완료

--- dtk-product-api/src/main/java/com/devtalk/product/productservice/product/application/port/in/dto/ProductReservedReq.java
+++ dtk-product-api/src/main/java/com/devtalk/product/productservice/product/application/port/in/dto/ProductReservedReq.java
@@ -2,6 +2,7 @@ package com.devtalk.product.productservice.product.application.port.in.dto;
 
 //import com.devtalk.product.productservice.product.domain.product.ReservedProceedType;
 import com.devtalk.product.productservice.product.domain.product.ProcessStatus;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -10,6 +11,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ProductReservedReq {
     private Long id; // id 필드 추가
     private Long productId;
